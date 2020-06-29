b6b786131c0475a280e79bea20ee64e9dac166e7
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-29 14:34:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,6 +18,9 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
     first_letter = text.scan(/\w+/)[0]
     if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+      true
+    else 
+      
 end
 
 def valid_phone_number?(phone)
